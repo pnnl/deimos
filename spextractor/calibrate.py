2cfa86767ac2a8bfa3ec9579e86eae522712302d
Sean Colby <seanmcolby@gmail.com>
2019-07-18 10:32:01 -0700

    adds tunemix calibration method

--- spextractor/calibrate.py
+++ spextractor/calibrate.py
@@ -1,5 +1,77 @@
 from scipy.stats import linregress
 import numpy as np
+import spextractor as spx
+import matplotlib.pyplot as plt
+
+
+def tunemix(path, mz, ccs, q, mz_tol=0.01, verbosity=0):
+    """
+    Calibrate using Agilent TuneMix.
+
+    Parameters
+    ----------
+    path : str
+        Path to TuneMix file stored as HDF.
+    mz : array
+        Calibration masses (m/z).
+    ccs : array
+        Calibration collision cross sections (A^2).
+    q : array
+        Calibration nomincal charges.
+    mz_tol : float, optional (default=0.01)
+        Tolerance in mass (m/z).
+    verbosity : int, optional (default=0).
+        0 - Silent operation.
+        1 - Prints found peak information.
+        2 - Additionally plots drift time peaks.
+
+    Returns
+    -------
+    measurements : dict
+        Experimental calibration points.
+    calibration :  ArrivalTimeCalibration instance
+        Calibration result.
+    """
+    data = spx.utils.load_hdf(path)
+    measurements = {'mz': [],
+                    'ccs': [],
+                    'q': [],
+                    'ta': []}
+    for mz_i, ccs_i, q_i in zip(mz, ccs, q):
+        # find peak by mz
+        peak = spx.targeted.find_feature(data, mz=mz_i, mz_tol=mz_tol)
+
+        if peak is not None:
+            # experimental mz
+            mz_exp = (peak['mz'] * peak['intensity'] / peak['intensity'].sum()).sum()
+
+            # dt peak
+            ta_exp = peak['drift_time'][peak['intensity'].idxmax()]
+
+            measurements['mz'].append(mz_exp)
+            measurements['ccs'].append(ccs_i)
+            measurements['q'].append(q_i)
+            measurements['ta'].append(ta_exp)
+
+            if verbosity > 0:
+                error = abs(mz_i - mz_exp) / mz_i * 1E6
+                print('reference mass:\t\t', mz_i)
+                print('experimental mass:\t', mz_exp)
+                print('mass error (ppm):\t', error)
+                print('experimental ta:\t', ta_exp)
+            if verbosity > 1:
+                spx.plot.drift_time(peak['drift_time'], peak['intensity'])
+                plt.show()
+        else:
+            if verbosity > 1:
+                print('reference mass:\t\t', mz_i)
+                print('peak not found.')
+
+    # calibration
+    c = ArrivalTimeCalibration()
+    c.calibrate(**measurements)
+
+    return measurements, c
 
 
 class ArrivalTimeCalibration:
@@ -36,15 +108,15 @@ class ArrivalTimeCalibration:
         if (self.beta is None) or (self.tfix is None):
             raise ValueError('Must perform calibration to yield beta and tfix.')
 
-    def calibrate(self, mass=None, ta=None, ccs=None, q=None,
+    def calibrate(self, mz=None, ta=None, ccs=None, q=None,
                   beta=None, tfix=None, buffer_mass=28.013):
         """
-        Performs calibration if 'mass', 'ta', 'ccs', and 'q' arrays are provided,
+        Performs calibration if 'mz', 'ta', 'ccs', and 'q' arrays are provided,
         otherwise calibration parameters 'beta' and 'tfix' must be supplied directly.
 
         Parameters
         ----------
-        mass : array, optional
+        mz : array, optional
             Calibration masses.
         ta : array, optional
             Calibration arrival times.
@@ -65,9 +137,14 @@ class ArrivalTimeCalibration:
         self.buffer_mass = buffer_mass
 
         # calibrant arrays supplied
-        if (mass is not None) and (ta is not None) and (ccs is not None) and (q is not None):
+        if (mz is not None) and (ta is not None) and (ccs is not None) and (q is not None):
+            mz = np.array(mz)
+            ta = np.array(ta)
+            ccs = np.array(ccs)
+            q = np.array(q)
+
             # linear regression
-            beta, tfix, r, p, se = linregress(np.sqrt(mass / (mass + self.buffer_mass)) * ccs / q, ta)
+            beta, tfix, r, p, se = linregress(np.sqrt(mz / (mz + self.buffer_mass)) * ccs / q, ta)
 
             # store params
             self.beta = beta
@@ -86,14 +163,14 @@ class ArrivalTimeCalibration:
 
         raise ValueError('Must supply arrays for calibration or calibration parameters.')
 
-    def arrival2ccs(self, mass, ta, q=1):
+    def arrival2ccs(self, mz, ta, q=1):
         """
-        Calculates collision cross section (CCS) from arrival time, mass,
+        Calculates collision cross section (CCS) from arrival time, mz,
         and nominal charge, according to calibration parameters.
 
         Parameters
         ----------
-        mass : float
+        mz : float
             Feature mass (m/z).
         ta : float
             Feature arrival time (ms).
@@ -106,16 +183,16 @@ class ArrivalTimeCalibration:
             Feature collision cross section (A^2).
         """
         self._check()
-        return q / self.beta * (ta - self.tfix) / np.sqrt(mass / (mass + self.buffer_mass))
+        return q / self.beta * (ta - self.tfix) / np.sqrt(mz / (mz + self.buffer_mass))
 
-    def ccs2arrival(self, mass, ccs, q=1):
+    def ccs2arrival(self, mz, ccs, q=1):
         """
-        Calculates arrival time from collsion cross section (CCS), mass,
+        Calculates arrival time from collsion cross section (CCS), mz,
         and nominal charge, according to calibration parameters.
 
         Parameters
         ----------
-        mass : float
+        mz : float
             Feature mass (m/z).
         ccs : float
             Feature collision cross section (A^2).
@@ -128,4 +205,4 @@ class ArrivalTimeCalibration:
             Feature arrival time (ms).
         """
         self._check()
-        return self.beta / q * np.sqrt(mass / (mass + self.buffer_mass)) * ccs + self.tfix
+        return self.beta / q * np.sqrt(mz / (mz + self.buffer_mass)) * ccs + self.tfix
--- spextractor/targeted.py
+++ spextractor/targeted.py
@@ -1,13 +1,24 @@
 
 
-def find_feature(ms1, ms2, mz=None, dt=None, mz_tol=6E-6, dt_tol=0.12):
-    ms1_f = ms1.loc[(ms1['mz'] <= mz + mz_tol) &
-                    (ms1['mz'] >= mz - mz_tol) &
-                    (ms1['drift_time'] <= dt + dt_tol) &
-                    (ms1['drift_time'] >= dt - dt_tol), :]
+def find_feature(ms, mz=None, dt=None, mz_tol=6E-6, dt_tol=0.12):
+    if (mz is not None) and (dt is not None):
+        feature = ms.loc[(ms['mz'] <= mz + mz_tol) &
+                         (ms['mz'] >= mz - mz_tol) &
+                         (ms['drift_time'] <= dt + dt_tol) &
+                         (ms['drift_time'] >= dt - dt_tol), :].reset_index(drop=True)
 
-    ms2_f = ms2.loc[(ms2['drift_time'] <= dt + dt_tol) &
-                    (ms2['drift_time'] >= dt - dt_tol) &
-                    (ms2['mz'] <= mz - mz_tol), :]  # not sure if we want this last one
+    elif mz is not None:
+        feature = ms.loc[(ms['mz'] <= mz + mz_tol) &
+                         (ms['mz'] >= mz - mz_tol), :].reset_index(drop=True)
 
-    return ms1_f, ms2_f
+    elif dt is not None:
+        feature = ms.loc[(ms['drift_time'] <= dt + dt_tol) &
+                         (ms['drift_time'] >= dt - dt_tol), :].reset_index(drop=True)
+
+    else:
+        raise ValueError('Either mz or dt must not be None.')
+
+    if len(feature.index) > 0:
+        return feature
+    else:
+        return None
