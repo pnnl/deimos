1f3d1cec7aaf8ddaa2ea741dcc72720733e10950
Sean Colby <seanmcolby@gmail.com>
2019-09-11 14:08:08 -0700

    debug print statements

--- spextractor/process.py
+++ spextractor/process.py
@@ -7,6 +7,7 @@ import spextractor as spx
 
 
 def mzml2hdf(path, output):
+    print('reading')
     # check for zip
     if splitext(path)[-1].lower() == 'mzml':
         f = path
@@ -22,34 +23,45 @@ def mzml2hdf(path, output):
         # close file
         f.close()
 
+    # parse
+    print('parsing with {} threads'.format(mp.cpu_count()))
     with mp.Pool(mp.cpu_count()) as p:
         parsed = p.map(_parse, data)
 
     # generate dataframe
+    print('casting to dataframe')
     df = pd.DataFrame(parsed, columns=['ms_level', 'drift_time', 'mz', 'intensity'])
 
     # explode m/z
+    print('exploding mz')
     a = df.set_index(['ms_level', 'drift_time'])['mz'].apply(pd.Series).stack()
     a = a.reset_index()
     a.columns = ['ms_level', 'drift_time', 'sample', 'mz']
 
     # explode intensity
+    print('exploding intensity')
     b = df.set_index(['ms_level', 'drift_time'])['intensity'].apply(pd.Series).stack()
     b = b.reset_index()
     b.columns = ['ms_level', 'drift_time', 'sample', 'intensity']
 
     # combine
+    print('merging mz and intensity')
     a['intensity'] = b['intensity'].values
     a.drop('sample', axis=1, inplace=True)
 
     # filter zero intensity out
+    print('filtering zeros')
     a = a.loc[a['intensity'] > 0, :]
 
     # group
+    print('grouping')
     a = a.groupby(by=['drift_time', 'mz', 'ms_level'], sort=False).sum().reset_index()
 
     # save
+    print('saving')
     spx.utils.save_hdf(a, output)
+
+    print('done')
 
 
 def _parse(d):
