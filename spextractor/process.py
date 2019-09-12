f42695cc4325494ec5f4f493a5b8e2a43df8ddae
Sean Colby <seanmcolby@gmail.com>
2019-09-12 09:49:29 -0700

    reduces memory footprint

--- spextractor/process.py
+++ spextractor/process.py
@@ -13,12 +13,12 @@ def mzml2hdf(path, output):
         f = path
 
         # process mzml
-        data = [x for x in mzml.read(f)]
+        data = mzml.read(f)
     else:
         f = gzip.open(path, 'rb')
 
         # process mzml
-        data = [x for x in mzml.read(f)]
+        data = mzml.read(f)
 
         # close file
         f.close()
@@ -29,37 +29,15 @@ def mzml2hdf(path, output):
         parsed = p.map(_parse, data)
 
     # generate dataframe
-    print('casting to dataframe')
-    df = pd.DataFrame(parsed, columns=['ms_level', 'drift_time', 'mz', 'intensity'])
-
-    # explode m/z
-    print('exploding mz')
-    a = df.set_index(['ms_level', 'drift_time'])['mz'].apply(pd.Series).stack()
-    a = a.reset_index()
-    a.columns = ['ms_level', 'drift_time', 'sample', 'mz']
-
-    # explode intensity
-    print('exploding intensity')
-    b = df.set_index(['ms_level', 'drift_time'])['intensity'].apply(pd.Series).stack()
-    b = b.reset_index()
-    b.columns = ['ms_level', 'drift_time', 'sample', 'intensity']
-
-    # combine
-    print('merging mz and intensity')
-    a['intensity'] = b['intensity'].values
-    a.drop('sample', axis=1, inplace=True)
-
-    # filter zero intensity out
-    print('filtering zeros')
-    a = a.loc[a['intensity'] > 0, :]
+    print('concatenating dataframes')
+    df = pd.concat(parsed)
 
     # group
-    print('grouping')
-    a = a.groupby(by=['drift_time', 'mz', 'ms_level'], sort=False).sum().reset_index()
+    df = df.groupby(by=['drift_time', 'mz', 'ms_level'], sort=False).sum().reset_index()
 
     # save
     print('saving')
-    spx.utils.save_hdf(a, output)
+    spx.utils.save_hdf(df, output)
 
     print('done')
 
@@ -68,4 +46,28 @@ def _parse(d):
     dt = [x['ion mobility drift time'] for x in d['scanList']['scan']]
     if len(dt) < 2:
         dt = dt[0]
-    return [d['ms level'], dt, d['m/z array'], d['intensity array']]
+
+    df = pd.DataFrame([d['ms level'], dt, d['m/z array'], d['intensity array']],
+                      columns=['ms_level', 'drift_time', 'mz', 'intensity'])
+
+    # explode m/z
+    a = df.set_index(['ms_level', 'drift_time'])['mz'].apply(pd.Series).stack()
+    a = a.reset_index()
+    a.columns = ['ms_level', 'drift_time', 'sample', 'mz']
+
+    # explode intensity
+    b = df.set_index(['ms_level', 'drift_time'])['intensity'].apply(pd.Series).stack()
+    b = b.reset_index()
+    b.columns = ['ms_level', 'drift_time', 'sample', 'intensity']
+
+    # combine
+    a['intensity'] = b['intensity'].values
+    a.drop('sample', axis=1, inplace=True)
+
+    # group
+    a = a.groupby(by=['drift_time', 'mz', 'ms_level'], sort=False).sum().reset_index()
+
+    # filter zero intensity out
+    a = a.loc[a['intensity'] > 0, :]
+
+    return a
