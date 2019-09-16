3c3d316350efff592e326df197482d1c1ab1306c
Sean Colby <seanmcolby@gmail.com>
2019-09-16 15:28:44 -0700

    adds retention time

--- spextractor/process.py
+++ spextractor/process.py
@@ -30,24 +30,33 @@ def mzml2hdf(path, output):
     df = pd.concat(parsed, ignore_index=True)
 
     # group
-    df = df.groupby(by=['drift_time', 'mz', 'ms_level'], sort=False).sum().reset_index()
+    df = df.groupby(by=['retention_time', 'drift_time', 'mz', 'ms_level'],
+                    sort=False).sum().reset_index()
 
     # save
     spx.utils.save_hdf(df, output)
 
 
 def _parse(d):
-    dt = [x['ion mobility drift time'] for x in d['scanList']['scan']]
-    if len(dt) < 2:
-        dt = dt[0]
+    try:
+        dt = [x['ion mobility drift time'] for x in d['scanList']['scan']][0]
+    except:
+        rt = np.nan
+
+    try:
+        rt = [x['scan start time'] for x in d['scanList']['scan']][0]
+    except:
+        rt = np.nan
 
     df = pd.DataFrame(data={'mz': d['m/z array'], 'intensity': d['intensity array']})
 
     df['ms_level'] = d['ms level']
     df['drift_time'] = dt
+    df['retention_time'] = rt
 
     # group
-    df = df.groupby(by=['drift_time', 'mz', 'ms_level'], sort=False).sum().reset_index()
+    df = df.groupby(by=['retention_time', 'drift_time', 'mz', 'ms_level'],
+                    sort=False).sum().reset_index()
 
     # filter zero intensity out
     df = df.loc[df['intensity'] > 0, :]
--- spextractor/utils.py
+++ spextractor/utils.py
@@ -1,55 +1,14 @@
 import h5py
 import pandas as pd
 import numpy as np
-import warnings
-
-
-dtype_dict = {'drift_time': np.float32,
-              'mz_ms1': np.float32,
-              'intensity_ms1': np.float32,
-              'mz_ms2': np.float32,
-              'intensity_ms2': np.float32,
-              'mz': np.float32,
-              'intensity': np.float32,
-              'ms_level': np.uint8}
-
-
-def types(keys):
-    return {k: dtype_dict[k] for k in keys}
 
 
 def save_hdf(df, path):
     with h5py.File(path, 'w') as f:
         for c in df.columns:
-            if c in dtype_dict.keys():
-                f.create_dataset(c, data=df[c].values.astype(dtype_dict[c]), compression_opts=9, compression="gzip")
-            else:
-                f.create_dataset(c, data=df[c].values, compression_opts=9, compression="gzip")
+            f.create_dataset(c, data=df[c].values, compression_opts=9, compression="gzip")
 
 
-def load_hdf(path, group=False):
+def load_hdf(path):
     with h5py.File(path, 'r') as f:
-        if group is False:
-            return pd.DataFrame({k: np.array(f[k]) for k in list(f.keys())}).astype(types(f.keys()))
-        elif group is True:
-            return _group(pd.DataFrame({k: np.array(f[k]) for k in list(f.keys())}).astype(types(f.keys())))
-        else:
-            raise KeyError(group)
-
-
-def _group(df):
-    # check all keys present
-    for key in ['drift_time', 'mz_ms1', 'intensity_ms1', 'mz_ms2', 'intensity_ms2']:
-        if key not in df.columns:
-            warnings.warn("This function requires a pandas data frame with columns \
-                          ['drift_time', 'mz_ms1', 'intensity_ms1', 'mz_ms2', 'intensity_ms2']")
-            raise KeyError(key)
-
-    # group
-    g = df.groupby(by=['drift_time',
-                       'mz_ms1',
-                       'intensity_ms1']).agg({'mz_ms2': lambda x: list(x),
-                                              'intensity_ms2': lambda x: list(x)}).reset_index()
-
-    # sort
-    return g.sort_values(by=['intensity_ms1', 'drift_time', 'mz_ms1'], ascending=[False, True, True])
+        return pd.DataFrame({k: np.array(f[k]) for k in list(f.keys())})
