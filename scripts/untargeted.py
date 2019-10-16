729028ac84effb41a033e1a85e8ea5ae8fd3df94
Sean Colby <seanmcolby@gmail.com>
2019-10-16 10:13:29 -0700

    untargeted in 2d

--- scripts/untargeted.py
+++ scripts/untargeted.py
@@ -25,44 +25,43 @@ def main(exp_path, output_path, beta, tfix, threshold):
     data['ms2'] = df.loc[df['ms_level'] == 2, :].drop('ms_level', axis=1).reset_index(drop=True)
 
     # find features
-    ms1_peaks = spx.peakpick.auto(data['ms1'], features=['mz', 'drift_time', 'retention_time'],
-                                  res=[0.01, 0.12, 0.05], sigma=[0.03, 0.3, 0.04], truncate=4, threshold=1000)
+    ms1_peaks = spx.peakpick.auto(ms1, features=['mz', 'drift_time'],
+                                  res=[0.01, 0.12], sigma=[0.03, 0.3], truncate=4, threshold=1000)
 
     # container
-    d = {'mz': [], 'rt': [], 'dt': [], 'ccs': [], 'intensity': [], 'ms2': []}
+    d = {'mz': [], 'dt': [], 'ccs': [], 'intensity': [], 'ms2_mz': [], 'ms2_intensity': []}
 
     # iterate through peaks
-    if ms1_peaks is not None:
-        for idx, peak in ms1_peaks.iterrows():
-            mz_exp = peak['mz']
-            rt_exp = peak['retention_time']
-            dt_exp = peak['drift_time']
-            int_exp = peak['intensity']
-            ccs_exp = c.arrival2ccs(mz_exp, dt_exp)
+    for idx, peak in ms1_peaks.iterrows():
+        mz_exp = peak['mz']
+        dt_exp = peak['drift_time']
+        int_exp = peak['intensity']
+        ccs_exp = c.arrival2ccs(dt_exp, mz_exp)
 
-            # extract ms2
-            ms2 = spx.targeted.find_feature(data['ms2'], by=['drift_time', 'retention_time'],
-                                            loc=[dt_exp, rt_exp],
-                                            tol=[4 * x for x in [0.03, 0.3, 0.04]])
+        # extract ms2
+        ms2_subset = spx.targeted.find_feature(ms2,
+                                               by='drift_time',
+                                               loc=dt_exp,
+                                               tol=4 * 0.3)
+        # ms2 peaks
+        if ms2_subset is not None:
+            ms2_mz = spx.utils.collapse(ms2_subset, keep='mz', how=np.sum)
 
-            if ms2 is not None:
-                ms2_mz = spx.utils.collapse(ms2, by='mz', how=np.sum)
+            # sort
+            ms2_out = ms2_mz.loc[(ms2_mz['intensity'] > 1000) & (ms2_mz['mz'] <= mz_exp + 10), :].sort_values(by='mz')
+            # ms2_out = ''.join(['%.4f %i;' % (mz, i) for mz, i in zip(ms2_out['mz'].values, ms2_out['intensity'].values)])
 
-                # sort
-                ms2_out = ms2_mz.loc[(ms2_mz['intensity'] > threshold) & (ms2_mz['mz'] <= mz_i + 10), :].sort_values(by='mz')
-                ms2_out = ''.join(['%.4f %i;' % (mz, i) for mz, i in zip(ms2_out['mz'].values, ms2_out['intensity'].values)])
+            # append
+            d['mz'].append(mz_exp)
+            d['dt'].append(dt_exp)
+            d['ccs'].append(ccs_exp)
+            d['intensity'].append(int_exp)
+            d['ms2_mz'].append(ms2_out['mz'].values)
+            d['ms2_intensity'].append(ms2_out['intensity'].values)
 
-                # append
-                d['mz'].append(mz_exp)
-                d['rt'].append(rt_exp)
-                d['dt'].append(dt_exp)
-                d['ccs'].append(ccs_exp)
-                d['intensity'].append(int_exp)
-                d['ms2'].append(ms2_out)
-
-    df = pd.DataFrame(d)
-    df = df.sort_values(by='intensity', ascending=False)
-    df.to_csv(join(output_path, '%s.tsv' % splitext(basename(exp_path))[0]), sep='\t', index=False)
+        df = pd.DataFrame(d)
+        df = df.sort_values(by='intensity', ascending=False)
+        df.to_csv(join(output_path, '%s.tsv' % splitext(basename(exp_path))[0]), sep='\t', index=False)
 
 
 if __name__ == '__main__':
