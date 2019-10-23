af75d7921943e160a54792e63ac1e338601a0376
Sean Colby <seanmcolby@gmail.com>
2019-10-23 09:20:02 -0700

    improvements to subsetting efficiency

--- spextractor/targeted.py
+++ spextractor/targeted.py
@@ -1,4 +1,5 @@
 from spextractor.utils import safelist, check_length
+import pandas as pd
 
 
 def find_feature(data, by=['mz', 'drift_time', 'retention_time'],
@@ -11,13 +12,34 @@ def find_feature(data, by=['mz', 'drift_time', 'retention_time'],
     # check dims
     check_length([by, loc, tol])
 
+    # extend columns
+    cols = data.columns
+    cidx = [cols.get_loc(x) for x in by]
+
     # subset by each dim
-    q = ' and '.join(['{} <= {} <= {}'.format(x - dx, feature, x + dx) for feature, x, dx in zip(by, loc, tol)])
-    data = data.query(q)
+    data = data.values
+    for i, x, dx in zip(cidx, loc, tol):
+        data = data[(data[:, i] <= x + dx) & (data[:, i] >= x - dx)]
 
     # data found
-    if len(data.index) > 0:
-        return data
+    if data.shape[0] > 0:
+        return pd.DataFrame(data, columns=cols)
 
     # no data
     return None
+
+
+if __name__ == '__main__':
+    import numpy as np
+    import time
+
+    data = pd.DataFrame(np.random.rand(10000, 4), columns=['mz', 'drift_time', 'retention_time', 'intensity'])
+    loc = [0.5, 0.5, 0.5]
+    tol = [0.1, 0.1, 0.1]
+
+    start = time.time()
+
+    find_feature(data, loc=loc, tol=tol)
+
+    stop = time.time()
+    print(stop - start)
