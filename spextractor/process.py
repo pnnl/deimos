750b2c1b58ec2a67d9b3dc77826f2fa0acb1250c
Sean Colby <seanmcolby@gmail.com>
2020-01-10 11:06:45 -0800

    removes unnecessary groupby operations

--- spextractor/process.py
+++ spextractor/process.py
@@ -23,17 +23,11 @@ def mzml2hdf(path, output):
 
     # parse in parallel
     with mp.Pool(mp.cpu_count()) as p:
-        # df = pd.DataFrame(np.concatenate([x for x in p.imap_unordered(_parse, data, chunksize=100)]),
-        #                   columns=['retention_time', 'drift_time', 'mz', 'ms_level', 'intensity'])
         df = pd.concat([x for x in p.imap_unordered(_parse, data, chunksize=1000)], ignore_index=True)
 
     # close zip file
     if zipped:
         f.close()
-
-    # groupby
-    df = dd.from_pandas(df, npartitions=mp.cpu_count())
-    df = df.groupby(by=['retention_time', 'drift_time', 'mz', 'ms_level', 'mz_precursor']).sum().reset_index().compute()
 
     # replace with nan
     df = df.replace(-1, np.nan)
@@ -86,16 +80,16 @@ def _parse(d):
     return df
 
 
-class HDFConcat:
-    def __init__(self, path):
-        self.path = path
-        self.store = pd.HDFStore(self.path)
+# class HDFConcat:
+#     def __init__(self, path):
+#         self.path = path
+#         self.store = pd.HDFStore(self.path)
 
-    def append(self, df):
-        self.store.append('data', df, data_columns=True)
+#     def append(self, df):
+#         self.store.append('data', df, data_columns=True)
 
-    def get(self):
-        df = self.store.select('data')
-        self.store.close()
-        os.remove(self.path)
-        return df
+#     def get(self):
+#         df = self.store.select('data')
+#         self.store.close()
+#         os.remove(self.path)
+#         return df
