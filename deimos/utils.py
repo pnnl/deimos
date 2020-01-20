31dba563a9b096f80234fb0cccacb0ff946ca5d8
Sean Colby <seanmcolby@gmail.com>
2020-01-20 12:48:45 -0800

    improvements to mzml reader

--- deimos/utils.py
+++ deimos/utils.py
@@ -1,109 +1,56 @@
 import h5py
 import pandas as pd
 import numpy as np
-from pyteomics import mzml
-import gzip
+import pymzml
 from os.path import *
-import multiprocessing as mp
 
 
-dtypes = {'mz': np.float32,
-          'intensity': np.uint32,
-          'retention_time': np.float32,
-          'drift_time': np.float32,
-          'ms_level': np.uint8}
+def read_mzml(path, accession={'drift_time': 'MS:1002476',
+                               'retention_time': 'MS:1000016'}):
+    # open file
+    data = pymzml.run.Reader(path)
+
+    # ordered dict
+    accession = OrderedDict(accession)
+
+    # result container
+    res = defaultdict(list)
+
+    # parse
+    for spec in data:
+        # init
+        arr = np.empty((spec.mz.shape[0], len(accession) + 2))
+        cols = ['mz', 'intensity']
+
+        # fill
+        arr[:, 0] = spec.mz
+        arr[:, 1] = spec.i
+
+        # drop
+        arr = arr[np.where(arr[:, 1] > 0)]
+
+        for i, (k, v) in enumerate(accession.items()):
+            cols.append(k)
+            arr[:, 2 + i] = spec.get(v)
+
+        res['ms{}'.format(spec.ms_level)].append(arr)
+
+    for level in res.keys():
+        res[level] = pd.DataFrame(np.concatenate(res[level], axis=0), columns=cols)
+
+    return res
 
 
-def read_mzml(path):
-    # check for zip
-    ext = splitext(path)[-1].lower()
-    if ext == '.gz':
-        zipped = True
-        f = gzip.open(path, 'rb')
-    else:
-        zipped = False
-        f = path
-
-    # mzml file handle
-    data = mzml.read(f)
-
-    # parse in parallel
-    with mp.Pool(mp.cpu_count()) as p:
-        df = pd.concat([x for x in p.imap_unordered(_parse, data, chunksize=1000)], ignore_index=True)
-
-    # close zip file
-    if zipped:
-        f.close()
-
-    # replace with nan
-    df = df.replace(-1, np.nan)
-
-    # drop missing axes
-    df = df.dropna(axis=1, how='all')
-
-    return df
-
-
-def _parse(d):
-    # drift time
-    try:
-        dt = d['scanList']['scan'][0]['ion mobility drift time']
-    except:
-        dt = -1
-
-    # retention time
-    try:
-        rt = d['scanList']['scan'][0]['scan start time']
-    except:
-        rt = -1
-
-    # precursor info
-    try:
-        precursor = d['precursorList']['precursor'][0]['selectedIonList']['selectedIon'][0]
-        pre_mz = precursor['selected ion m/z']
-        pre_int = precursor['peak intensity']
-    except:
-        pre_mz = -1
-        pre_int = -1
-
-    df = pd.DataFrame(data={'mz': d['m/z array'], 'intensity': d['intensity array']})
-
-    df['ms_level'] = d['ms level']
-    df['drift_time'] = dt
-    df['retention_time'] = rt
-    df['mz_precursor'] = pre_mz
-    df['intensity_precursor'] = pre_int
-
-    # filter zero intensity out
-    df = df.loc[df['intensity'] > 0, :]
-
-    # return df.values
-    return df
-
-
-def dtype(k):
-    if k in dtypes.keys():
-        return dtypes[k]
-    else:
-        return np.float32
-
-
-def save_hdf(df, path, compression_level=5):
-    idx = np.array(df['ms_level'] == 1)
+def save_hdf(path, data, dtype={}, compression_level=5):
     with h5py.File(path, 'w') as f:
-        for level in ['ms1', 'ms2']:
+        for level in data.keys():
             f.create_group(level)
+            for c in data[level].columns:
+                if c not in dtypes.keys():
+                    dtype[c] = np.float32
 
-        for c in df.columns:
-            if c == 'ms_level':
-                pass
-            else:
-                f['ms1'].create_dataset(c, data=df.loc[idx, c].values,
-                                        dtype=dtype(c),
-                                        compression="gzip",
-                                        compression_opts=compression_level)
-                f['ms2'].create_dataset(c, data=df.loc[~idx, c].values,
-                                        dtype=dtype(c),
+                f[level].create_dataset(c, data=data[level][c].values,
+                                        dtype=dtype[c],
                                         compression="gzip",
                                         compression_opts=compression_level)
 
