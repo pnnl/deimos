368201b70c4b677775b85aaee864d51a51ac4415
Sean Colby <seanmcolby@gmail.com>
2020-02-05 15:48:59 -0800

    adds slice method

--- deimos/targeted.py
+++ deimos/targeted.py
@@ -27,3 +27,30 @@ def find_feature(data, by=['mz', 'drift_time', 'retention_time'],
 
     # no data
     return None
+
+
+def slice(data, by=['mz', 'drift_time', 'retention_time'],
+          low=[0, 0, 0], high=[0, 0, 0]):
+    # safely cast to list
+    by = safelist(by)
+    low = safelist(low)
+    high = safelist(high)
+
+    # check dims
+    check_length([by, low, high])
+
+    # extend columns
+    cols = data.columns
+    cidx = [cols.get_loc(x) for x in by]
+
+    # subset by each dim
+    data = data.values
+    for i, lb, ub in zip(cidx, low, high):
+        data = data[(data[:, i] <= ub) & (data[:, i] >= lb)]
+
+    # data found
+    if data.shape[0] > 0:
+        return pd.DataFrame(data, columns=cols)
+
+    # no data
+    return None
