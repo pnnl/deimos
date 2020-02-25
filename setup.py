8a4fc980f32a7041780b1ca1686c4917631e4139
Sean Colby <seanmcolby@gmail.com>
2020-02-25 15:08:38 -0800

    adds license

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
-
 from setuptools import setup, find_packages
 from deimos import __version__
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
 
 pkgs = find_packages(exclude=('examples', 'docs', 'resources'))
 
@@ -8,9 +13,11 @@ setup(
     name='deimos',
     version=__version__,
     description='Data Extraction for Integrated Multidimensional Spectrometry',
+    long_description=readme,
     author='Sean M. Colby',
     author_email='sean.colby@pnnl.gov',
     url='https://github.com/pnnl/deimos',
+    license=license,
     packages=pkgs,
     entry_points={
         'console_scripts': ['deimos = deimos.cli:main']
