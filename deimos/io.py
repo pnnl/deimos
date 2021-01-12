d90611d47273eff794425a4671f13ca63bbe6a8c
Sean Colby <seanmcolby@gmail.com>
2021-01-12 15:05:04 -0800

    Reorganizes `utils` module Moves file input/output functions to `io` module. Moves data wrangling operations (slice, partition, etc.) to `subset` module. Deletes legacy `find_standard` functions. Renames `find_feature` function to `locate`. The `utils` module became to much of a catch-all for functionality, and this restructuring hopefully improves layout and intuitiveness.

---
+++ deimos/io.py
@@ -0,0 +1,186 @@
+import pymzml
+import h5py
+from collections import OrderedDict, defaultdict
+import warnings
+import pandas as pd
+
+
+def read_mzml(path, accession={'drift_time': 'MS:1002476',
+                               'retention_time': 'MS:1000016'}):
+    """
+    Read in an mzML file, parsing for accession values, to yield
+    a long-format data frame.
+
+    Parameters
+    ----------
+    path : str
+        Path to input mzML file.
+    accession : dict
+        Key-value pairs signaling which features to parse for
+        in the mzML file.
+
+    Returns
+    -------
+    out : DataFrame
+        Parsed feature coordinates and intensities.
+
+    """
+
+    # open file
+    data = pymzml.run.Reader(path)
+
+    # ordered dict
+    accession = OrderedDict(accession)
+
+    # result container
+    res = defaultdict(list)
+
+    # precursor rename
+    pdict = {'mz': 'precursor_mz',
+             'i': 'precursor_intensity',
+             'charge': 'precursor_charge'}
+
+    # parse
+    for spec in data:
+        # dimension check
+        if len(spec.mz) != len(spec.i):
+            warnings.warn("m/z and intensity array dimension mismatch")
+            continue
+
+        # init
+        cols = ['mz', 'intensity']
+
+        # check for precursor
+        if spec.selected_precursors:
+            arr = np.empty((spec.mz.shape[0],
+                            len(accession) + len(spec.selected_precursors[0]) + 2), dtype=np.float32)
+
+        # no precursor
+        else:
+            arr = np.empty((spec.mz.shape[0], len(accession) + 2), dtype=np.float32)
+
+        # fill
+        arr[:, 0] = spec.mz
+        arr[:, 1] = spec.i
+
+        # populate accession fields
+        for i, (k, v) in enumerate(accession.items()):
+            cols.append(k)
+            arr[:, 2 + i] = spec.get(v)
+
+        # populate precursor information
+        if spec.selected_precursors:
+            for i, (k, v) in enumerate(spec.selected_precursors[0].items()):
+                cols.append(pdict[k])
+                arr[:, 2 + len(accession) + i] = v
+
+        # append dataframe
+        res['ms{}'.format(spec.ms_level)].append(pd.DataFrame(arr, columns=cols))
+
+    # concatenate dataframes
+    for level in res.keys():
+        res[level] = pd.concat(res[level], axis=0, ignore_index=True)
+
+    return res
+
+
+def save_hdf(path, data, dtype={}, compression_level=5):
+    """
+    Saves data frame to HDF5 container.
+
+    Parameters
+    ----------
+    path : str
+        Path to output file.
+    data : dict
+        Dictionary of data frames to be saved. Dictionary keys
+        will be saved as 'groups' (e.g., MS level) and
+        data frame columns will be saved as 'datasets'
+        in the HDF5 container.
+    dtype : dict
+        Specifies what data type to save each column, provided as column:dtype
+        pairs. Defaults to 32-bit float if unspecified.
+    compression_level : int
+        A value from 0-9 signaling the number of compression operations to apply.
+        Higher values result in greater compression at the expense of computational
+        overhead.
+
+    Returns
+    -------
+    None.
+
+    """
+
+    with h5py.File(path, 'w') as f:
+        for level in data.keys():
+            f.create_group(level)
+            for c in data[level].columns:
+                if c not in dtype.keys():
+                    dtype[c] = np.float32
+
+                f[level].create_dataset(c, data=data[level][c].values,
+                                        dtype=dtype[c],
+                                        compression="gzip",
+                                        compression_opts=compression_level)
+
+
+def load_hdf(path, level='ms1'):
+    """
+    Loads data frame from HDF5 container.
+
+    Parameters
+    ----------
+    path : str
+        Path to input HDF5 file.
+    level : str
+        Access this level (group) of the HDF5 container.
+        E.g., 'ms1' or 'ms2' for MS levels 1 or 2,
+        respectively.
+
+    Returns
+    -------
+    out : DataFrame
+        Feature coordinates and intensities for the
+        specified level.
+
+    """
+
+    with h5py.File(path, 'r') as f:
+        g = f[level]
+        return pd.DataFrame({k: g[k] for k in list(g.keys())})
+
+
+def save_mgf(path, data, charge='1+'):
+    """
+    Saves data to MGF format.
+
+    Parameters
+    ----------
+    path : str
+        Path to output file.
+    data : DataFrame
+        Precursor m/z and intensities paired to
+        MS2 spectra.
+
+    Returns
+    -------
+    None.
+
+    """
+
+    template = ('BEGIN IONS\n'
+                'PEPMASS={} {}\n'
+                'CHARGE={}\n'
+                'TITLE=Spectrum {}\n'
+                '{}\n'
+                'END IONS\n')
+
+    with open(path, 'w') as f:
+        for i, row in data.iterrows():
+            precursor_mz = row['mz']
+            precursor_int = row['intensity']
+            ms2 = row['ms2']
+
+            # check for ms2 spectra
+            if ms2 is not np.nan:
+                f.write(template.format(precursor_mz, precursor_int, charge, i, ms2.replace(';', '\n')))
---
+++ deimos/subset.py
@@ -0,0 +1,403 @@
+from deimos.utils import safelist, check_length
+import pandas as pd
+import numpy as np
+from functools import partial
+import multiprocessing as mp
+
+
+def threshold(data, by='intensity', threshold=0):
+    """
+    Thresholds input by intensity.
+
+    Parameters
+    ----------
+    data : DataFrame
+        Input feature coordinates and intensities.
+    by : str
+        Variable to threshold by.
+    threshold : float
+        Intensity threshold value.
+
+    Returns
+    -------
+    out : DataFrame
+        Thresholded feature coordinates.
+
+    """
+
+    return data.loc[data[by] > threshold, :].reset_index(drop=True)
+
+
+def collapse(data, keep=['mz', 'drift_time', 'retention_time'], how=np.sum):
+    """
+    Collpases input data such that only specified dimensions remain
+    according to the supplied aggregation function.
+
+    Parameters
+    ----------
+    data : DataFrame
+        Input feature coordinates and intensities.
+    keep : str or list
+        Features to keep during collapse operation.
+    how : func
+        Aggregation function for collapse operation.
+
+    Returns
+    -------
+    out : DataFrame
+        Collapsed feature coordinates and aggregated
+        intensities.
+
+    """
+
+    return data.groupby(by=keep, as_index=False, sort=False).agg({'intensity': how})
+
+
+def locate(data, by=['mz', 'drift_time', 'retention_time'],
+           loc=[0, 0, 0], tol=[0, 0, 0], return_index=False):
+    """
+    Given a feature coordinate and tolerances, return a subset
+    of the data.
+
+    Parameters
+    ----------
+    data : DataFrame
+        Input feature coordinates and intensities.
+    by : str or list
+        Feature(s) by which to subset the data
+    loc : float or list
+        Coordinate location.
+    tol : float or list
+        Tolerance in each dimension.
+    return_index : bool
+        Return indices of subset.
+
+    Returns
+    -------
+    out : DataFrame
+        Subset of feature coordinates and intensities.
+    index : array
+        If `return_index` is True, indices of slice elements.
+
+    """
+
+    # safely cast to list
+    by = safelist(by)
+    loc = safelist(loc)
+    tol = safelist(tol)
+
+    # check dims
+    check_length([by, loc, tol])
+
+    if data is None:
+        if return_index is True:
+            return None, None
+        else:
+            return None
+
+    # store index
+    rindex = data.index.values
+
+    # extend columns
+    cols = data.columns
+    cidx = [cols.get_loc(x) for x in by]
+
+    # subset by each dim
+    data = data.values
+    idx = np.full(data.shape[0], True, dtype=bool)
+    for i, x, dx in zip(cidx, loc, tol):
+        idx *= (data[:, i] <= x + dx) & (data[:, i] >= x - dx)
+
+    data = data[idx]
+    rindex = rindex[idx]
+
+    if return_index is True:
+        # data found
+        if data.shape[0] > 0:
+            return pd.DataFrame(data, index=rindex, columns=cols), idx
+
+        # no data
+        return None, idx
+    else:
+        # data found
+        if data.shape[0] > 0:
+            return pd.DataFrame(data, index=rindex, columns=cols)
+
+        # no data
+        return None
+
+
+def slice(data, by=['mz', 'drift_time', 'retention_time'],
+          low=[0, 0, 0], high=[0, 0, 0], return_index=False):
+    """
+    Given a feature coordinate and bounds, return a subset
+    of the data.
+
+    Parameters
+    ----------
+    data : DataFrame
+        Input feature coordinates and intensities.
+    by : str or list
+        Feature(s) by which to subset the data
+    low : float or list
+        Lower bound(s) in each dimension.
+    high : float or list
+        Upper bound(s) in each dimension.
+    return_index : bool
+        Return indices of subset.
+
+    Returns
+    -------
+    out : DataFrame
+        Subset of feature coordinates and intensities.
+    index : array
+        If `return_index` is True, indices of slice elements.
+
+    """
+
+    # safely cast to list
+    by = safelist(by)
+    low = safelist(low)
+    high = safelist(high)
+
+    # check dims
+    check_length([by, low, high])
+
+    if data is None:
+        if return_index is True:
+            return None, None
+        else:
+            return None
+
+    # store index
+    rindex = data.index.values
+
+    # extend columns
+    cols = data.columns
+    cidx = [cols.get_loc(x) for x in by]
+
+    # subset by each dim
+    data = data.values
+    idx = np.full(data.shape[0], True, dtype=bool)
+    for i, lb, ub in zip(cidx, low, high):
+        idx *= (data[:, i] <= ub) & (data[:, i] >= lb)
+
+    data = data[idx]
+    rindex = rindex[idx]
+
+    if return_index is True:
+        # data found
+        if data.shape[0] > 0:
+            return pd.DataFrame(data, index=rindex, columns=cols), idx
+
+        # no data
+        return None, idx
+    else:
+        # data found
+        if data.shape[0] > 0:
+            return pd.DataFrame(data, index=rindex, columns=cols)
+
+        # no data
+        return None
+
+
+class Partitions:
+    """
+    Generator object that will lazily build and
+    return each partition.
+
+    Parameters
+    ----------
+    data : DataFrame
+        Input feature coordinates and intensities.
+    split_on : str
+        Dimension to partition the data.
+    size : int
+        Target partition size.
+    overlap : float
+        Amount of overlap between partitions to ameliorate edge effects.
+
+    """
+
+    def __init__(self, data, split_on='mz', size=1000, overlap=0.05):
+        self.data = data
+        self.split_on = split_on
+        self.size = size
+        self.overlap = overlap
+
+        self._compute_splits()
+
+    def _compute_splits(self):
+        """
+        Determines data splits for partitioning.
+
+        """
+
+        # unique to split on
+        idx = np.unique(self.data[self.split_on].values)
+
+        # number of partitions
+        partitions = np.ceil(len(idx) / self.size)
+
+        # determine partition bounds
+        bounds = [[x.min(), x.max()] for x in np.array_split(idx, partitions)]
+        for i in range(1, len(bounds)):
+            bounds[i][0] = bounds[i - 1][1] - self.overlap
+
+        if (self.overlap > 0) & (len(bounds) > 1):
+            # functional bounds
+            fbounds = []
+            for i in range(len(bounds)):
+                a, b = bounds[i]
+
+                # first partition
+                if i < 1:
+                    b = b - self.overlap / 2
+
+                # middle partitions
+                elif i < len(bounds) - 1:
+                    a = a + self.overlap / 2
+                    b = b - self.overlap / 2
+
+                # last partition
+                else:
+                    a = a + self.overlap / 2
+
+                fbounds.append([a, b])
+        else:
+            fbounds = bounds
+
+        self.bounds = bounds
+        self.fbounds = fbounds
+
+    def __iter__(self):
+        """
+        Yields each partition.
+
+        Yields
+        ------
+        out : DataFrame
+            Partition of feature coordinates and intensities.
+
+        """
+
+        for a, b in self.bounds:
+            yield slice(self.data, by=self.split_on, low=a, high=b)
+
+    def map(self, func, processes=1, **kwargs):
+        """
+        Maps `func` to each partition, then returns the combined result,
+        accounting for overlap regions.
+
+        Parameters
+        ----------
+        func : function
+            Function to apply to partitions.
+        processes : int
+            Number of parallel processes. If less than 2,
+            a serial mapping is applied.
+        kwargs
+            Keyword arguments passed to `func`.
+
+        Returns
+        -------
+        out : DataFrame
+            Combined result of `func` applied to partitions.
+
+        """
+
+        # serial
+        if processes < 2:
+            result = [func(x, **kwargs) for x in self]
+
+        # parallel
+        else:
+            with mp.Pool(processes=processes) as p:
+                result = list(p.imap(partial(func, **kwargs), self))
+
+        # reconcile overlap
+        result = [slice(result[i], by=self.split_on, low=a, high=b)
+                  for i, (a, b) in enumerate(self.fbounds)]
+
+        # combine partitions
+        return pd.concat(result).reset_index(drop=True)
+
+    def zipmap(self, func, b, processes=1, **kwargs):
+        """
+        Maps `func` to each partition pair resulting from the zip operation
+        of `self` and `b`, then returns the combined result, accounting
+        for overlap regions.
+
+        Parameters
+        ----------
+        func : function
+            Function to apply to zipped partitions. Must accept and
+            return two DataFrames.
+        b : DataFrame
+            Input feature coordinates and intensities.
+        processes : int
+            Number of parallel processes. If less than 2,
+            a serial mapping is applied.
+        kwargs
+            Keyword arguments passed to `func`.
+
+        Returns
+        -------
+        a, b : DataFrame
+            Combined result of `func` applied to partitions.
+
+        """
+
+        # partition other dataset
+        partitions = (slice(b, by=self.split_on, low=a, high=b_)
+                      for a, b_ in self.bounds)
+
+        # serial
+        if processes < 2:
+            result = [func(a, b_, **kwargs) for a, b_ in zip(self, partitions)]
+
+        # parallel
+        else:
+            with mp.Pool(processes=processes) as p:
+                result = list(p.starmap(partial(func, **kwargs), zip(self, partitions)))
+
+        result = {'a': [x[0] for x in result], 'b': [x[1] for x in result]}
+
+        # reconcile overlap
+        tmp = [slice(result['a'][i], by=self.split_on, low=a, high=b_, return_index=True)
+               for i, (a, b_) in enumerate(self.fbounds)]
+        result['a'] = [x[0] for x in tmp]
+        idx = [x[1] for x in tmp]
+        result['b'] = [p.iloc[i, :] if i is not None else None for p, i in zip(result['b'], idx)]
+
+        # combine partitions
+        result['a'] = pd.concat(result['a'])
+        result['b'] = pd.concat(result['b'])
+
+        return result['a'], result['b']
+
+
+def partition(data, split_on='mz', size=1000, overlap=0.05):
+    """
+    Partitions data along a feature dimension.
+
+    Parameters
+    ----------
+    data : DataFrame
+        Input feature coordinates and intensities.
+    split_on : str
+        Dimension to partition the data.
+    size : int
+        Target partition size.
+    overlap : float
+        Amount of overlap between partitions to ameliorate edge effects.
+
+    Returns
+    -------
+    out : Partitions
+        A generator object that will lazily build and
+        return each partition.
+
+    """
+
+    return Partitions(data, split_on, size, overlap)
