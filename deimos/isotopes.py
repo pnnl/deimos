3449c409ca8e1e07b28e7957779cfedd198a4a7d
Sean Colby <seanmcolby@gmail.com>
2021-01-12 14:34:51 -0800

    Implements isotope detection Initial commit of the isotope detection module. Still needs docstrings and to expose _all_ configurable variables, largely dependent on conforming other modules to share intensity definitions (i.e. "intensity" versus "sum_2" as apex and 2 sigma integral intensity, respectively).

---
+++ deimos/isotopes.py
@@ -0,0 +1,85 @@
+import deimos
+import glob
+import numpy as np
+import pandas as pd
+import scipy
+import scipy.ndimage as ndi
+
+
+def OrderedSet(x):
+    return list({k: None for k in x})
+
+
+def detect(data, features=['mz', 'drift_time', 'retention_time'], tol=[0.1, 0.2, 0.3],
+           delta=1.003355, max_isotopes=4, max_charge=1):
+    
+    # safely cast to list
+    features = deimos.utils.safelist(features)
+    tol = deimos.utils.safelist(tol)
+    
+    # check dims
+    deimos.utils.check_length([features, tol])
+    
+    # isolate mz dimension
+    mz_idx = features.index('mz')
+    else_idx = [i for i, j in enumerate(features) if i != mz_idx]
+
+    isotopes = []
+    idx = []
+    for i in else_idx:
+        arr = data[features[i]].values.reshape((-1, 1))
+        d = scipy.spatial.distance.cdist(arr, arr)
+
+        # less than tolerance
+        idx.append(d <= tol[i])
+
+    # stack truth arrays
+    idx = np.prod(np.dstack(idx), axis=-1)
+
+    # half matrix
+    idx = np.tril(idx, k=-1)
+
+    # isotopic distances
+    arr = data[features[mz_idx]].values.reshape((-1, 1))
+    d = scipy.spatial.distance.cdist(arr, arr)
+    d = np.multiply(d, idx)
+
+    for charge in range(1, max_charge + 1):
+        for mult in range(1, max_isotopes + 1):
+
+            dx_i = mult * (delta / charge) 
+            r, c = np.where((d > dx_i - tol[mz_idx]) & (d < dx_i + tol[mz_idx]))
+            a = data.iloc[c, :]
+            b = data.iloc[r, :]
+            z = charge * np.ones(len(a))
+            m = mult * np.ones(len(a))
+            dx_i = dx_i * np.ones(len(a))
+
+            isotopes.append(pd.DataFrame(np.vstack((a['mz'].values,
+                                                    a['sum_2'].values,
+                                                    z,
+                                                    m,
+                                                    dx_i,
+                                                    b['mz'].values,
+                                                    b['sum_2'].values,
+                                                    a['idx'].values,
+                                                    b['idx'].values)).T,
+                                         columns=['mz', 'intensity', 'charge', 'multiple', 'dx', 'mz_iso', 'intensity_iso', 'idx', 'idx_iso']))
+    
+    # combine
+    isotopes = pd.concat(isotopes, axis=0, ignore_index=True)
+    
+    # stats
+    isotopes['error'] = 1E6 * np.abs((isotopes['mz_iso'] - isotopes['mz']) - isotopes['dx']) / isotopes['mz']
+    isotopes['decay'] = isotopes['intensity_iso'] / isotopes['intensity']
+    
+    # cull non-decreasing
+    isotopes = isotopes.loc[isotopes['intensity'] > isotopes['intensity_iso'], :]
+    
+    # cull high error
+    isotopes = isotopes.loc[isotopes['error'] < 50, :]
+    
+    # cull children
+    isotopes = isotopes.loc[~isotopes['idx'].isin(isotopes['idx_iso']), :]
+                            
+    return isotopes
