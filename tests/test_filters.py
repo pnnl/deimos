0a3460baa7cb2b5e6e952d55a6079e160c558db2
Sean Colby <seanmcolby@gmail.com>
2021-04-19 12:52:16 -0700

    Add tests for filters module

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,50 +1,63 @@
 import pytest
 import deimos
+import numpy as np
 from tests import localfile
 
 
-@pytest.fixture()
-def ms1():
-    return deimos.load_hdf(localfile('resources/example_data.h5'),
-                           level='ms1')
+@pytest.mark.parametrize('array,size,expected',
+                         [(np.arange(5), 5, [1, 1, 1.414, 1.414, 2])])
+def test_stdev(array, size, expected):
+    result = deimos.filters.stdev(array, size)
+    assert (np.abs(result - expected) <= 1E-3).all()
 
 
-@pytest.fixture()
-def ms2():
-    return deimos.load_hdf(localfile('resources/example_data.h5'),
-                           level='ms2')
+@pytest.mark.parametrize('array,size,expected',
+                         [(np.arange(5), 5, [2, 3, 4, 4, 4])])
+def test_maximum(array, size, expected):
+    result = deimos.filters.maximum(array, size)
+    assert (result == expected).all()
 
 
-def test_stdev():
-    raise NotImplementedError
+@pytest.mark.parametrize('array,size,expected',
+                         [(np.arange(5), 5, [0, 0, 0, 0, 0])])
+def test_minimum(array, size, expected):
+    result = deimos.filters.minimum(array, size)
+    assert (result == expected).all()
 
 
-def test_maximum():
-    raise NotImplementedError
+@pytest.mark.parametrize('array,size,expected',
+                         [(np.arange(5), 5, [0, 5, 10, 10, 5])])
+def test_sum(array, size, expected):
+    result = deimos.filters.sum(array, size)
+    assert (result == expected).all()
 
 
-def test_minimum():
-    raise NotImplementedError
+@pytest.mark.parametrize('array,size,expected',
+                         [(np.arange(5), 5, [0.4, 0.6, 0.8, 0.8, 0.6])])
+def test_mean(array, size, expected):
+    result = deimos.filters.mean(array, size)
+    assert (result == expected).all()
 
 
-def test_sum():
-    raise NotImplementedError
+@pytest.mark.parametrize('array,size,expected',
+                         [(np.arange(5), 5, [0, 0, 0, 0, 0])])
+def test_matched_gaussian(array, size, expected):
+    result = deimos.filters.matched_gaussian(array, size)
+    assert (result == expected).all()
 
 
-def test_mean():
-    raise NotImplementedError
+@pytest.mark.parametrize('array,size,expected',
+                         [(np.arange(5), 5, [0, 1, 2, 2, 0.25])])
+def test_signal_to_noise_ratio(array, size, expected):
+    result = deimos.filters.signal_to_noise_ratio(array, size)
+    assert (np.abs(result - expected) <= 1E-3).all()
 
 
-def test_matched_gaussian():
-    raise NotImplementedError
-
-
-def test_signal_to_noise_ratio():
-    raise NotImplementedError
-
-
-def test_count():
-    raise NotImplementedError
+@pytest.mark.parametrize('array,size,expected',
+                         [(np.arange(5), 5, [3, 4, 5, 4, 3])])
+def test_count(array, size, expected):
+    result = deimos.filters.count(array, size)
+    assert (result == expected).all()
 
 
 def test_kurtosis():
