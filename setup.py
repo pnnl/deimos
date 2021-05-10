88a74a982b6fcec12f23073d5b6a2434a2fde394
Sean Colby <seanmcolby@gmail.com>
2021-05-10 11:24:11 -0700

    Updates pip install

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-from deimos import __version__
 
 with open('README.md') as f:
     readme = f.read()
@@ -7,16 +6,21 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
+with open('requirements.txt') as f:
+    requirements = f.read()
+
 pkgs = find_packages(exclude=('examples', 'docs', 'resources'))
 
 setup(
     name='deimos',
-    version=__version__,
+    version='0.1.0',
     description='Data Extraction for Integrated Multidimensional Spectrometry',
     long_description=readme,
     author='Sean M. Colby',
     author_email='sean.colby@pnnl.gov',
     url='https://github.com/pnnl/deimos',
+    install_requires=requirements,
+    python_requires='>=3.7',
     license=license,
     packages=pkgs,
     entry_points={
