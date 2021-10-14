f8783949adfc9ddf47f22bce82d2756572f95448
Sean Colby <seanmcolby@gmail.com>
2021-10-14 13:50:34 -0700

    Add test stubs for deconvolution module

---
+++ tests/test_deconvolution.py
@@ -0,0 +1,28 @@
+import deimos
+import numpy as np
+import pandas as pd
+import pytest
+from tests import localfile
+
+
+def test_get_1D_profiles():
+    raise NotImplementedError
+
+
+class TestMS2Deconvolution:
+
+    def test_init(self):
+        raise NotImplementedError
+    
+    def test_cluster(self):
+        raise NotImplementedError
+
+    def test_configure_profile_extraction(self):
+        raise NotImplementedError
+
+    def test_apply(self):
+        raise NotImplementedError
+
+
+def test_deconvolve_ms2():
+    raise NotImplementedError
