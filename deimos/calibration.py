9d8e804e8517582e488bbdd904b7ca8624ae8779
jessbade <jessicabade01@gmail.com>
2021-10-26 17:20:48 -0700

    Adding ms2 drift calibration functionality

--- deimos/calibration.py
+++ deimos/calibration.py
@@ -225,7 +225,6 @@ def tunemix(features,
     '''
     Provided tune mix data with known calibration ions (i.e. known m/z, CCS, and nominal charge),
     determine the arrival time for each to define a CCS calibration.
-
     Parameters
     ----------
     mz : :obj:`~numpy.array`
@@ -240,23 +239,21 @@ def tunemix(features,
         Tolerance in ppm to isolate tune ion.
     dt_tol : float
         Fractional tolerance to define drift time window bounds.
-
     Returns
     -------
     :obj:`~deimos.calibration.ArrivalTimeCalibration`
         Instance of calibrated `~deimos.calibration.ArrivalTimeCalibration`
         object.
-
     '''
 
     # cast to numpy array
     mz = np.array(mz)
     ccs = np.array(ccs)
     q = np.array(q)
-    
+
     # check lengths
     deimos.utils.check_length([mz, ccs, q])
-    
+
     # iterate tune ions
     ta = []
     for mz_i, ccs_i, q_i in zip(mz, ccs, q):
@@ -264,23 +261,178 @@ def tunemix(features,
         subset = deimos.slice(features, by='mz',
                               low=mz_i - 0.1 * mz_tol,
                               high=mz_i + mz_i * 0.9 * mz_tol)
-        
+
         # extract dt info
         dt_profile = deimos.collapse(subset, keep='drift_time')
-        dt_i = dt_profile.sort_values(by='intensity', ascending=False)['drift_time'].values[0]   
-        dt_profile = deimos.locate(dt_profile, by='drift_time', loc=dt_i, tol=dt_tol * dt_i).sort_values(by='drift_time')
-        
+        dt_i = dt_profile.sort_values(by='intensity', ascending=False)['drift_time'].values[0]
+        dt_profile = deimos.locate(dt_profile, by='drift_time', loc=dt_i,
+                                   tol=dt_tol * dt_i).sort_values(by='drift_time')
+
         # interpolate spline
         x = dt_profile['drift_time'].values
         y = dt_profile['intensity'].values
-        
+
         spl = interp1d(x, y, kind='quadratic')
         newx = np.arange(x.min(), x.max(), 0.001)
-        newy = spl(newx)        
+        newy = spl(newx)
         dt_j = newx[np.argmax(newy)]
-        
+
         ta.append(dt_j)
-    
+
     # calibrate
     ta = np.array(ta)
     return deimos.calibration.calibrate_ccs(mz=mz, ta=ta, ccs=ccs, q=q, buffer_mass=buffer_mass)
+
+
+class ms2DriftCalibration:
+
+    def __init__(self):
+        '''
+        Initializes :obj:`~deimos.calibration.ms2DriftCalibration` object.
+
+        '''
+
+        # initialize variables
+        self.mode = None
+        self.mz = None
+        self.df = None
+        self.b = None
+
+    def set_mode(self, mode=None):
+        '''
+        Parameters
+        ----------
+        mode : string
+            String of mode of ionization; must be specified as:
+                negative, neg, -, positive, pos, or +
+        '''
+        self.mode = mode
+        return
+
+    def set_mz(self, mz=None):
+        '''
+        Parameters
+        ----------
+        mz : list
+            List of float mz values to use in calibration of drift time
+        '''
+        if mz is None:
+            if self.mode.lower() in ['positive', 'pos', '+']:
+                self.mz = np.array([118.086255, 322.048121, 622.028960,
+                                    922.009798, 1221.990636, 1521.971475])
+            elif self.mode.lower() in ['negative', 'neg', '-']:
+                self.mz = np.array([112.985587, 301.998139, 601.978977,
+                                    1033.988109, 1333.968947, 1633.949786])
+        else:
+            self.mz = np.array(mz)
+        return
+
+    def subset_tune(self, decon_data, orbitrap=False):
+        '''
+        Provided data with known calibration ions (i.e. known m/z),
+        subset the deconvolution output by those ions.
+
+        Parameters
+        ----------
+
+
+        Returns
+        -------
+
+        '''
+        decon_data['bias'] = 1
+
+        meta_df = pd.DataFrame(columns=decon_data.columns)
+        for mz_i in mz:
+            subset = decon_data[decon_data['mz_ms1'].between(mz_i-2, mz_i+2)]
+            if orbitrap == True:
+                # TODO compare to orbitrap here
+                # subset['OrbitrapScore'] = cosine_score()
+            subset2 = subset[subset['mz_ms2'].between(mz_i-2, mz_i+2)]
+            meta_df = meta_df.append(subset2)
+        self.df = meta_df
+        return
+
+    def regress_tune(self):
+        '''
+
+        Parameters
+        ----------
+
+        Returns
+        -------
+
+
+        Raises
+        ------
+
+
+        '''
+
+        df = self.df.copy()
+        x = df['drift_time_ms2'].values
+        y = df['drift_time_ms1'].values
+        z = df['voltage'].values
+        bias = df['bias'].values
+        X = np.hstack(x, z, bias)
+        b = np.linalg.pinv(X).dot(y)
+        self.b = b
+        return
+
+
+def tunemix_calibrate(decon_data, mode, **kwargs):
+    '''
+    Parameters
+    ----------
+    decon_data : pd.DataFrame
+        output from `deimos.calibration.stitch_deconvolutions`
+    '''
+    mdc = ms2DriftCalibration()
+    mdc.set_mode(mode)
+    mdc.set_mz()
+    mdc.subset_tune(decon_data, **kwargs)
+    mdc.regress_tune()
+    return mdc
+
+
+def add_voltage_to_deconvolution(decon_data, voltage):
+    '''
+    Parameters
+    ----------
+    decon_data : pd.DataFrame
+        deimos.deconvolution.deconvolve_ms2 output
+    voltage : int
+        int value of voltage
+    '''
+    return decon_data['voltage'] = voltage
+
+
+def stitch_deconvolutions(list_of_decons):
+    '''
+    Parameters
+    ----------
+    list_of_decons : list of pd.DataFrame
+        list of `deimos.calibration.add_voltage_to_deconvolution` output
+    '''
+    return pd.concat(list_of_decons)
+
+
+def shift_drift(ms2, mdc):
+    '''
+    Provided an ms2 or ms2_peaks object, update the drift_time column with the calibration object.
+
+    Parameters
+    ----------
+    ms2 : pd.DataFrame
+        (1) deimos.load_hdf(key='ms2') output or
+        (2) deimos.peakpick.local_maxima() output or
+        (3) deimos.threshold() output
+
+    Returns
+    -------
+    ms2 : pd.DataFrame
+        same as input with modified `drift_time` column
+    '''
+    ms2_df = ms2.copy()
+    ms2['drift_time'] = ms2['drift_time'].apply(lambda dt: [dt, voltage].dot(tmc.b))
+    return ms2
