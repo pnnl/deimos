2f81b537b272f6ddde48a2fb1a919ee67a8d0e39
Sean Colby <seanmcolby@gmail.com>
2021-12-03 10:31:05 -0800

    Specify explicit namespace imports

--- deimos/__init__.py
+++ deimos/__init__.py
@@ -3,11 +3,11 @@ from deimos import calibration
 from deimos import deconvolution
 from deimos import filters
 from deimos import grid
-from deimos.io import *
+from deimos.io import read_mzml, save_hdf, load_hdf, load_hdf_multi, save_mgf
 from deimos import isotopes
 from deimos import peakpick
 from deimos import plot
-from deimos.subset import *
+from deimos.subset import threshold, collapse, locate, locate_asym, slice, partition, multi_sample_partition
 from deimos import utils
 
 __version__ = "0.1.0"
