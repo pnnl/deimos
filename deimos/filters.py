e6f3bd99eea4e9a9ecc09ca7234518b693d55fd6
Sean Colby <seanmcolby@gmail.com>
2022-04-04 13:53:25 -0700

    Remove kurtosis, snr filters pending bugfixes

--- deimos/filters.py
+++ deimos/filters.py
@@ -139,39 +139,6 @@ def matched_gaussian(a, size):
     return np.square(ndi.gaussian_filter(a, size, mode='constant', cval=0.0))
 
 
-def snr(a, size):
-    '''
-    N-dimensional convolution of a signal-to-noise filter.
-
-    Parameters
-    ----------
-    a : :obj:`~numpy.array`
-        N-dimensional array of intensity data.
-    size : int or list
-        Size of the convolution kernel in each dimension.
-
-    Returns
-    -------
-    :obj:`~numpy.array`
-        Filtered intensity data.
-
-    '''
-
-    # get min
-    mn = ndi.minimum_filter(a, size=size, mode='constant', cval=np.inf)
-    
-    # get median
-    median = ndi.median_filter(a, size=size, mode='nearest')
-    
-    # noise basis
-    floor = 2 * (median - mn)
-
-    # compute snr
-    return np.square(np.divide(a - mn, floor,
-                               out=np.ones_like(a),
-                               where=floor != 0))
-
-
 def count(a, size, nonzero=False):
     '''
     N-dimensional convolution of a counting filter.
@@ -198,60 +165,3 @@ def count(a, size, nonzero=False):
         a = np.where(np.isnan(a), 0.0, 1.0)
 
     return deimos.filters.sum(a, size)
-
-
-def kurtosis(edges, a, size):
-    '''
-    N-dimensional convolution of a kurtosis filter. Kurtosis
-    is determined per dimension.
-
-    Parameters
-    ----------
-    edges : list of :obj:`~numpy.array`
-        Edge coordinates along each grid axis.
-    a : :obj:`~numpy.array`
-        N-dimensional array of intensity data.
-    size : int or list
-        Size of the convolution kernel in each dimension.
-
-    Returns
-    -------
-    :obj:`~numpy.array`
-        Filtered intensity data.
-
-    '''
-
-    # argument checks
-    edges = deimos.utils.safelist(edges)
-    size = deimos.utils.safelist(size)
-    deimos.utils.check_length([edges, size, a.shape])
-
-    # result container
-    k = []
-
-    # define edges on grid
-    edges = np.meshgrid(*edges)
-
-    # enumerate dims
-    for i, (e, s) in enumerate(zip(edges, size)):
-        freq = ndi.uniform_filter1d(a, size=s, axis=i)
-
-        # frequency sum
-        total = s * ndi.uniform_filter(freq, s, mode='constant', cval=0.0)
-
-        # x-weighted sum
-        xbar = s * ndi.uniform_filter(e * freq, s,
-                                      mode='constant', cval=0.0) / total
-
-        # second moment
-        m2 = s * ndi.uniform_filter(np.square(e - xbar) * freq, s,
-                                    mode='constant', cval=0.0) / total
-
-        # fourth moment
-        m4 = s * ndi.uniform_filter(np.power(e - xbar, 4) * freq, s,
-                                    mode='constant', cval=0.0) / total
-
-        # kurtosis calculation
-        k.append(m4 / np.square(m2) - 3.0)
-
-    return k
--- tests/test_filters.py
+++ tests/test_filters.py
@@ -5,11 +5,16 @@ from scipy import signal
 
 
 @pytest.fixture()
-def gaussian_3d():
+def g3d():
     return 10E4 * np.einsum('i,j,k',
                             signal.windows.gaussian(70, 3),
                             signal.windows.gaussian(70, 3),
                             signal.windows.gaussian(70, 3))
+
+
+@pytest.fixture()
+def g1d():
+    return 10E4 * signal.windows.gaussian(70, 3)
 
 
 @pytest.mark.parametrize('array,size,expected',
@@ -54,27 +59,8 @@ def test_matched_gaussian(array, size, expected):
     assert (np.abs(result - expected) <= 1E-3).all()
 
 
-# @pytest.mark.parametrize('array,size,expected',
-#                          [(np.arange(5.), 5, [0.5625, 1.05882353, 2., 2., 1.265625])])
-# def test_signal_to_noise_ratio(array, size, expected):
-#     result = deimos.filters.signal_to_noise_ratio(array, size)
-#     assert (np.abs(result - expected) <= 1E-3).all()
-
-
 @pytest.mark.parametrize('array,size,expected',
                          [(np.arange(5.), 5, [3, 4, 5, 4, 3])])
 def test_count(array, size, expected):
     result = deimos.filters.count(array, size)
     assert (result == expected).all()
-
-
-@pytest.mark.parametrize('size,expected',
-                         [([69, 69, 69], 0)])
-def test_kurtosis(gaussian_3d, size, expected):
-    edges = [np.arange(gaussian_3d.shape[x]) for x in range(gaussian_3d.ndim)]
-    result = deimos.filters.kurtosis(edges, gaussian_3d, size)
-
-    for dim in result:
-        # compare center of array to expectation
-        # (because evaluation will be of a Gaussian at center)
-        assert np.abs(np.take(dim, dim.size // 2) - expected) < 1E-3
