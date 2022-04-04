54908c8bbb6219c9686b1ed2369869e90008277b
Sean Colby <seanmcolby@gmail.com>
2022-04-04 13:40:11 -0700

    Apply bugfix for kurtosis, consistent formatting

--- deimos/filters.py
+++ deimos/filters.py
@@ -1,10 +1,9 @@
 import deimos
 import numpy as np
 import scipy.ndimage as ndi
-from itertools import cycle
 
 
-def stdev(a, size):
+def std(a, size):
     '''
     N-dimensional convolution of a standard deviation filter.
 
@@ -22,8 +21,8 @@ def stdev(a, size):
 
     '''
 
-    c1 = ndi.filters.uniform_filter(a, size, mode='constant')
-    c2 = ndi.filters.uniform_filter(np.square(a), size, mode='constant')
+    c1 = ndi.uniform_filter(a, size, mode='reflect')
+    c2 = ndi.uniform_filter(np.square(a), size, mode='reflect')
     return np.abs(np.lib.scimath.sqrt(c2 - np.square(c1)))
 
 
@@ -45,7 +44,7 @@ def maximum(a, size):
 
     '''
 
-    return ndi.maximum_filter(a, size=size, mode='constant', cval=0)
+    return ndi.maximum_filter(a, size=size, mode='constant', cval=-np.inf)
 
 
 def minimum(a, size):
@@ -66,7 +65,7 @@ def minimum(a, size):
 
     '''
 
-    return ndi.minimum_filter(a, size=size, mode='constant', cval=0)
+    return ndi.minimum_filter(a, size=size, mode='constant', cval=np.inf)
 
 
 def sum(a, size):
@@ -88,12 +87,14 @@ def sum(a, size):
     '''
 
     size = deimos.utils.safelist(size)
+
     if len(size) == 1:
         size = size[0]
-        ksize = size ** len(a.shape)
+        ksize = size ** a.ndim
     else:
         ksize = np.prod(size)
-    return ksize * ndi.filters.uniform_filter(a, size, mode='constant')
+
+    return ksize * ndi.uniform_filter(a, size, mode='constant', cval=0.0)
 
 
 def mean(a, size):
@@ -114,8 +115,7 @@ def mean(a, size):
 
     '''
 
-    # a = np.where(np.nan_to_num(a) > 0, 1.0, 0.0)
-    return ndi.uniform_filter(a, size=size, mode='constant', cval=0)
+    return ndi.uniform_filter(a, size=size, mode='constant', cval=0.0)
 
 
 def matched_gaussian(a, size):
@@ -136,31 +136,40 @@ def matched_gaussian(a, size):
 
     '''
 
-    return np.square(ndi.gaussian_filter(a, size, mode='constant', cval=0))
+    return np.square(ndi.gaussian_filter(a, size, mode='constant', cval=0.0))
 
 
-# def signal_to_noise_ratio(a, size):
-#     '''
-#     N-dimensional convolution of a signal-to-noise filter.
+def snr(a, size):
+    '''
+    N-dimensional convolution of a signal-to-noise filter.
 
-#     Parameters
-#     ----------
-#     a : :obj:`~numpy.array`
-#         N-dimensional array of intensity data.
-#     size : int or list
-#         Size of the convolution kernel in each dimension.
+    Parameters
+    ----------
+    a : :obj:`~numpy.array`
+        N-dimensional array of intensity data.
+    size : int or list
+        Size of the convolution kernel in each dimension.
 
-#     Returns
-#     -------
-#     :obj:`~numpy.array`
-#         Filtered intensity data.
+    Returns
+    -------
+    :obj:`~numpy.array`
+        Filtered intensity data.
 
-#     '''
+    '''
 
-#     c1 = ndi.filters.uniform_filter(a, size, mode='constant')
-#     c2 = ndi.filters.uniform_filter(np.square(a), size, mode='constant')
-#     std = np.abs(np.lib.scimath.sqrt(c2 - np.square(c1)))
-#     return np.square(np.divide(c1, std, out=np.zeros_like(c1, dtype=float), where=std != 0))
+    # get min
+    mn = ndi.minimum_filter(a, size=size, mode='constant', cval=np.inf)
+    
+    # get median
+    median = ndi.median_filter(a, size=size, mode='nearest')
+    
+    # noise basis
+    floor = 2 * (median - mn)
+
+    # compute snr
+    return np.square(np.divide(a - mn, floor,
+                               out=np.ones_like(a),
+                               where=floor != 0))
 
 
 def count(a, size, nonzero=False):
@@ -212,24 +221,37 @@ def kurtosis(edges, a, size):
 
     '''
 
+    # argument checks
     edges = deimos.utils.safelist(edges)
     size = deimos.utils.safelist(size)
     deimos.utils.check_length([edges, size, a.shape])
 
+    # result container
     k = []
-    for i, (e, s) in enumerate(zip(edges, size)):
-        ax = tuple(x for x in range(len(edges)) if x is not i)
-        freq = np.sum(a, axis=ax)
 
-        # conv
-        total = s * ndi.filters.uniform_filter(freq, s, mode='constant')
-        xbar = s * ndi.filters.uniform_filter(e * freq, s,
-                                              mode='constant') / total
-        m2 = s * ndi.filters.uniform_filter(np.square(e - xbar) * freq, s,
-                                            mode='constant') / total
-        m4 = s * ndi.filters.uniform_filter(np.power(e - xbar, 4) * freq, s,
-                                            mode='constant') / total
+    # define edges on grid
+    edges = np.meshgrid(*edges)
+
+    # enumerate dims
+    for i, (e, s) in enumerate(zip(edges, size)):
+        freq = ndi.uniform_filter1d(a, size=s, axis=i)
+
+        # frequency sum
+        total = s * ndi.uniform_filter(freq, s, mode='constant', cval=0.0)
+
+        # x-weighted sum
+        xbar = s * ndi.uniform_filter(e * freq, s,
+                                      mode='constant', cval=0.0) / total
+
+        # second moment
+        m2 = s * ndi.uniform_filter(np.square(e - xbar) * freq, s,
+                                    mode='constant', cval=0.0) / total
+
+        # fourth moment
+        m4 = s * ndi.uniform_filter(np.power(e - xbar, 4) * freq, s,
+                                    mode='constant', cval=0.0) / total
+
+        # kurtosis calculation
         k.append(m4 / np.square(m2) - 3.0)
 
-    k = np.meshgrid(*k, indexing='ij')
     return k
