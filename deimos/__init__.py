e9e316f824f9832bdd2ef2cb27d11652fb0631ad
Sean Colby <seanmcolby@gmail.com>
2022-07-30 08:04:53 -0700

    Uptick patch version

--- deimos/__init__.py
+++ deimos/__init__.py
@@ -4,4 +4,4 @@ from deimos.io import get_accessions, load, save
 from deimos.subset import (collapse, locate, locate_asym,
                            multi_sample_partition, partition, slice, threshold)
 
-__version__ = "1.2.2"
+__version__ = "1.2.3"
