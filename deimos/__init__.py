63238939d7ebb8495eb900bfbffa4247e987a52a
Sean Colby <sean.colby@pnnl.gov>
2023-05-16 11:53:26 -0700

    Uptick patch version

--- deimos/__init__.py
+++ deimos/__init__.py
@@ -4,4 +4,4 @@ from deimos.io import get_accessions, load, save, build_factors, build_index
 from deimos.subset import (collapse, locate, locate_asym,
                            multi_sample_partition, partition, slice, threshold)
 
-__version__ = "1.3.1"
+__version__ = "1.3.2"
